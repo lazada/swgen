75899bdf6ab7c62f5a9e2d3891f7e6fdc4feca96
Sergey Bogomolov <sergey.bogomolov@lazada.com>
2017-07-07 15:30:24 +0300

    [-] removed function ReflectRealTypeName() and test for it (function was temporarily added, but not used in final implementation)

--- helper.go
+++ helper.go
@@ -5,18 +5,6 @@ import (
 	"fmt"
 )
 
-func ReflectRealTypeName(i interface{}) string {
-	v := reflect.ValueOf(i)
-	for v.Type().Kind() == reflect.Ptr || v.Type().Kind() == reflect.Interface {
-		if v.IsNil() {
-			v = reflect.New(v.Type().Elem()).Elem()
-		} else {
-			v = v.Elem()
-		}
-	}
-	return v.Type().String()
-}
-
 func ReflectTypeHash(t reflect.Type) uint32 {
 	return uint32(reflect.Indirect(reflect.ValueOf(t)).FieldByName("hash").Uint())
 }
--- helper_test.go
+++ helper_test.go
@@ -15,41 +15,6 @@ type TestStruct2 struct {
 	Name string
 }
 
-const expectedStructName string = "swgen.TestStruct1"
-
-func testReflectRealTypeName(t *testing.T) {
-	var (
-		t1 TestStruct1
-		t2 *TestStruct1
-		t3 interface{} = t1
-		t4 interface{} = t2
-		t5             = &t3
-		t6             = &t4
-	)
-
-	test := func(i interface{}, testName string) {
-		var typeName string
-		defer func() {
-			if r := recover(); r != nil {
-				t.Log("Panic on", testName, r)
-			}
-		}()
-		if typeName = ReflectRealTypeName(i); typeName != expectedStructName {
-			t.Errorf("Test failed on %s: expected '%s', got '%s'.", testName, expectedStructName, typeName)
-		} else {
-			t.Logf("Test %s ok: returned '%s', Type.Name() = '%s', Type.String() = '%s'",
-				testName, typeName, reflect.TypeOf(i).Name(), reflect.TypeOf(i).String())
-		}
-	}
-
-	test(t1, "t1")
-	test(t2, "t2")
-	test(t3, "t3")
-	test(t4, "t4")
-	test(t5, "t5")
-	test(t6, "t6")
-}
-
 func TestReflectTypeHash(t *testing.T) {
 	var (
 		ts1a, ts1b TestStruct1
